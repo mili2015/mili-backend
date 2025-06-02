7f2163916b97ec91c5cecb560d5a8a606d989a02
Felipe Alves <vieira.felipe.alves@gmail.com>
2025-06-02 15:17:54 -0300

    fix: 42391 | fix port - deploy jenkins

--- src/main/java/br/com/mili/milibackend/fornecedor/adapter/web/GfdDocumentoController.java
+++ src/main/java/br/com/mili/milibackend/fornecedor/adapter/web/GfdDocumentoController.java
@@ -1,30 +1,16 @@
 package br.com.mili.milibackend.fornecedor.adapter.web;
 
-import br.com.mili.milibackend.fornecedor.application.dto.FornecedorGetAllInputDto;
-import br.com.mili.milibackend.fornecedor.application.dto.FornecedorGetAllOutputDto;
-import br.com.mili.milibackend.fornecedor.application.dto.FornecedorMeusDadosUpdateInputDto;
-import br.com.mili.milibackend.fornecedor.application.dto.FornecedorMeusDadosUpdateOutputDto;
 import br.com.mili.milibackend.fornecedor.application.dto.gfdDocumento.GfdDocumentoDownloadInputDto;
 import br.com.mili.milibackend.fornecedor.application.dto.gfdDocumento.GfdDocumentoDownloadOutputDto;
 import br.com.mili.milibackend.fornecedor.application.dto.gfdDocumento.GfdDocumentoUpdateInputDto;
 import br.com.mili.milibackend.fornecedor.application.dto.gfdDocumento.GfdDocumentoUpdateOutputDto;
-import br.com.mili.milibackend.fornecedor.application.service.FornecedorService;
-import br.com.mili.milibackend.fornecedor.domain.interfaces.service.IFornecedorService;
 import br.com.mili.milibackend.gfd.domain.interfaces.IGfdDocumentoService;
 import br.com.mili.milibackend.shared.infra.security.model.CustomUserPrincipal;
-import br.com.mili.milibackend.shared.page.pagination.MyPage;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
-import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Set;
-import java.util.stream.Collectors;
-
-import static br.com.mili.milibackend.shared.roles.GfdRolesConstants.ROLE_ANALISTA;
 
 @Slf4j
 @RestController
