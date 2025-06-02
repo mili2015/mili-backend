29b66caa009c764ce067c00cee3d0cbf6e05de83
Luiz Henrique Pinheiro Silva <luizhenriquepinheirosilva@hotmail.com>
2025-06-02 08:54:17 -0300

    feat: 42055 implements gfd fornecedor

--- src/test/java/br/com/mili/milibackend/MiliBackendApplicationTests.java
+++ src/test/java/br/com/mili/milibackend/MiliBackendApplicationTests.java
@@ -1,13 +1,18 @@
 package br.com.mili.milibackend;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MiliBackendApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Value("${spring.profiles.active}")
+    private String profile;
 
+    @Test
+    void test_profile() {
+        Assertions.assertEquals("prod", profile, "ATENÇÃO! Profile definido como dev mas deveria ser prod");
+    }
 }
