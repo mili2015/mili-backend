6e24056b2de8a4ca9b0b2a8ec6919d16f98e6d80
Luiz Henrique Pinheiro Silva <luizhenriquepinheirosilva@hotmail.com>
2025-09-24 16:29:14 -0300

    fix: 45320 folder name

--- src/main/java/br/com/mili/milibackend/gfd/domain/usecases/GfdFuncionario/CreateFuncionarioUseCase.java
+++
@@ -1,8 +0,0 @@
-package br.com.mili.milibackend.gfd.domain.usecases.GfdFuncionario;
-
-import br.com.mili.milibackend.gfd.application.dto.gfdFuncionario.GfdFuncionarioCreateInputDto;
-import br.com.mili.milibackend.gfd.application.dto.gfdFuncionario.GfdFuncionarioCreateOutputDto;
-
-public interface CreateFuncionarioUseCase {
-    GfdFuncionarioCreateOutputDto execute(GfdFuncionarioCreateInputDto inputDto);
-}
