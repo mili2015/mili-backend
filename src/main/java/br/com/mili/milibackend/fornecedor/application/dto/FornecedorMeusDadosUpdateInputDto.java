9f7bf2e094b25e85608b795254e6c13ceb754220
Luiz Henrique Pinheiro Silva <luizhenriquepinheirosilva@hotmail.com>
2025-09-24 11:26:18 -0300

    feat 44889: implement update tipofornecedor | fix: 45372 add system 82 in auth to get roles

--- src/main/java/br/com/mili/milibackend/fornecedor/application/dto/FornecedorMeusDadosUpdateInputDto.java
+++ src/main/java/br/com/mili/milibackend/fornecedor/application/dto/FornecedorMeusDadosUpdateInputDto.java
@@ -4,10 +4,7 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 
@@ -15,6 +12,7 @@ import java.util.List;
 @AllArgsConstructor
 @Getter
 @Setter
+@Builder
 public class FornecedorMeusDadosUpdateInputDto {
     private Integer id;
     private Integer codUsuario;
@@ -38,6 +36,7 @@ public class FornecedorMeusDadosUpdateInputDto {
     @NoArgsConstructor
     @Getter
     @Setter
+    @Builder
     public static class GfdTipoFornecedorDto {
         @NotNull
         private Integer id;
--- src/main/java/br/com/mili/milibackend/gfd/application/dto/gfdTipoFornecedor/GfdTipoFornecedorGetByIdOutputDto.java
+++ src/main/java/br/com/mili/milibackend/gfd/application/dto/gfdTipoFornecedor/GfdTipoFornecedorGetByIdOutputDto.java
@@ -1,14 +1,12 @@
 package br.com.mili.milibackend.gfd.application.dto.gfdTipoFornecedor;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class GfdTipoFornecedorGetByIdOutputDto {
     private Integer id;
     private String descricao;
