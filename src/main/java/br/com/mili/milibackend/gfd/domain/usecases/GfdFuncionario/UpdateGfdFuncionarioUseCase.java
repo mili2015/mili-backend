f4484f937264cf4b004dcfe9f92d7776b481a032
Luiz Henrique Pinheiro Silva <luizhenriquepinheirosilva@hotmail.com>
2025-09-24 15:50:23 -0300

    fix 45320 folder

--- src/main/java/br/com/mili/milibackend/gfd/domain/usecases/GfdFuncionario/UpdateGfdFuncionarioUseCase.java
+++ src/main/java/br/com/mili/milibackend/gfd/domain/usecases/GfdFuncionario/UpdateGfdFuncionarioUseCase.java
@@ -1,4 +1,4 @@
-package br.com.mili.milibackend.gfd.domain.usecases.gfdFuncionario;
+package br.com.mili.milibackend.gfd.domain.usecases.GfdFuncionario;
 
 import br.com.mili.milibackend.gfd.application.dto.gfdFuncionario.GfdFuncionarioUpdateInputDto;
 import br.com.mili.milibackend.gfd.application.dto.gfdFuncionario.GfdFuncionarioUpdateOutputDto;
