6b410297c5a1624d197afe0bf86fbcfc802c308a
Luiz Henrique Pinheiro Silva <luizhenriquepinheirosilva@hotmail.com>
2025-10-02 17:53:41 -0300

    fix: handle new exception in filter 'funcionar' for null ctfor_codigo

--- src/main/java/br/com/mili/milibackend/gfd/adapter/exception/GfdFuncionarioCodeException.java
+++ src/main/java/br/com/mili/milibackend/gfd/adapter/exception/GfdFuncionarioCodeException.java
@@ -7,6 +7,7 @@ public enum GfdFuncionarioCodeException {
 
     GFD_FUNCIONARIO_NAO_ENCONTRADO("GFD_FUNCIONARIO_NAO_ENCONTRADO", "O funcionário não foi encontrado."),
     GFD_FUNCIONARIO_JA_DESLIGADO("GFD_FUNCIONARIO_JA_DESLIGADO", "O funcionário já esta desligado."),
+    GFD_FORNECEDOR_NULL("GFD_FORNECEDOR_NULL", "É necessário informar o fornecedor"),
     GFD_FUNCIONARIO_LIBERACAO_COM_DOCUMENTO_PENDENTE("GFD_FUNCIONARIO_LIBERACAO_COM_DOCUMENTO_PENDENTE", "Existes documentos pendentes, caso deseje liberar o funcionário, informe a justificativa");
 
     private final String code;
